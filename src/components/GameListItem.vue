cbbaa4fc351a0fe4bfaa576f61edb38464fa4467
Jonathan Andersson <ja1984@gmail.com>
2020-04-03 14:11:50 +0200

    Add more game descriptions

--- src/components/GameListItem.vue
+++ src/components/GameListItem.vue
@@ -1,6 +1,33 @@
 <template>
-  <div class="game-list-item">
-
+  <div class="card">
+    <header class="card__header">
+      <div class="game-image">
+        <img
+          :src="`https://raw.githubusercontent.com/nilicule/StadiaGameDB/master/images/posters/webp/${game.image_slug}.webp`"
+          class="game-image__image"
+        />
+        <div class="rating" v-if="game.rating && !isNaN(game.rating)" alt="Rating" title="Rating">
+          <img src="@/assets/smile.svg" class="rating__icon" />
+          <span class="rating__text">{{ game.rating || 'n/a' }}</span>
+        </div>
+      </div>
+    </header>
+    <section class="card__body">
+      <span class="game__name">{{ game.name }}</span>
+      <p>{{ game.description }}</p>
+    </section>
+    <footer class="card__footer">
+      <div class="row row--center-v">
+        <div class="column">
+          <span class="release-date">{{ game.formatRelease }}</span>
+        </div>
+        <div class="column column--wrap">
+          <a :href="game.store_link" target="_blank">
+            <img src="@/assets/shopping-bag.svg" />
+          </a>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -10,12 +37,63 @@ export default {
   props: {
     game: {
       type: Object,
-      default: () => {},
+      default: () => { },
     },
   },
 };
 </script>
 
 <style lang="less" scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+}
+
+.card__body {
+  flex: 1;
+}
+
+.game__name {
+  font-weight: 600;
+  font-size: 17px;
+}
+
+.release-date {
+  color: #aaa;
+}
+
+.game-image {
+  position: relative;
+}
+
+.rating {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: rgba(0,0,0, .7);
+  border-radius: 40px;
+  padding: 3px 6px;
+  color: #fff;
+  display: flex;
+}
+
+.rating__icon {
+  height: 18px;
+  display: inline-block;
+  vertical-align: sub;
+  margin-right: 3px;
+}
+
+.rating__text {
+  font-size: 13px;
+  line-height: 18px;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+
+.game-image__image {
+  display: block;
+  width: 100%;
+}
 
 </style>
--- src/components/GamesLibrary.vue
+++ src/components/GamesLibrary.vue
@@ -58,33 +58,7 @@
       <div class="games-wrapper">
         <div class="row">
           <div class="column column--small game" v-for="game in orderedGames" :key="game.name">
-            <div class="card">
-              <header class="card__header">
-                <div class="game-image">
-                  <img :src="`https://raw.githubusercontent.com/nilicule/StadiaGameDB/master/images/posters/webp/${game.image_slug}.webp`" class="game-image__image">
-                  <div class="rating" v-if="game.rating && !isNaN(game.rating)" alt="Rating" title="Rating">
-                    <img src="@/assets/smile.svg" class="rating__icon">
-                    <span class="rating__text">
-                      {{ game.rating || 'n/a' }}
-                    </span>
-                  </div>
-                </div>
-              </header>
-              <section class="card__body">
-                <span class="game__name">{{ game.name }}</span>
-                <p>{{ game.description }}</p>
-              </section>
-              <footer class="card__footer">
-                <div class="row row--center-v">
-                  <div class="column">
-                    <span class="release-date">{{ game.formatRelease }}</span>
-                  </div>
-                  <div class="column column--wrap">
-                    <a :href="game.store_link" target="_blank"><img src="@/assets/shopping-bag.svg"></a>
-                  </div>
-                </div>
-              </footer>
-            </div>
+            <game-list-item :game="game"></game-list-item>
           </div>
         </div>
       </div>
@@ -96,6 +70,8 @@
 import axios from 'axios';
 import { VueSlideToggle } from 'vue-slide-toggle';
 
+import GameListItem from '@/components/GameListItem.vue';
+
 
 export default {
   name: 'GamesLibrary',
@@ -104,6 +80,7 @@ export default {
   },
   components: {
     VueSlideToggle,
+    GameListItem,
   },
   data() {
     return {
@@ -187,49 +164,6 @@ a {
   color: #42b983;
 }
 
-.game-image {
-  position: relative;
-}
-
-.rating {
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  background: rgba(0,0,0, .7);
-  border-radius: 40px;
-  padding: 3px 6px;
-  color: #fff;
-  display: flex;
-}
-
-.rating__icon {
-  height: 18px;
-  display: inline-block;
-  vertical-align: sub;
-  margin-right: 3px;
-}
-
-.rating__text {
-  font-size: 13px;
-  line-height: 18px;
-  font-weight: bold;
-  text-transform: uppercase;
-}
-
-.game-image__image {
-  display: block;
-  width: 100%;
-}
-
-.card {
-  display: flex;
-  flex-direction: column;
-}
-
-.card__body {
-  flex: 1;
-}
-
 
 .filter {
   width: 100%;
@@ -239,10 +173,6 @@ a {
   }
 }
 
-.game__name {
-  font-weight: 600;
-  font-size: 17px;
-}
 
 .filter-genre, .filter-player {
   flex: 0 1 20%;
@@ -270,10 +200,6 @@ a {
 }
 
 
-.release-date {
-  color: #aaa;
-}
-
 .fade-enter-active {
   transition: opacity .3s;
 }
@@ -286,18 +212,6 @@ a {
   margin: 10px 0;
 }
 
-.game {
-  flex: 0 1 100%;
-
-  @media(min-width: 768px) {
-    flex: 0 1 50%;
-  }
-
-  @media(min-width: 1200px) {
-    flex: 0 1 100%/3;
-  }
-}
-
 .header .row {
   display: block;
 
@@ -308,5 +222,17 @@ a {
 
 .header .logo {
   display: flex;
+}
+
+.game {
+  flex: 0 1 100%;
+
+  @media (min-width: 768px) {
+    flex: 0 1 50%;
+  }
+
+  @media (min-width: 1200px) {
+    flex: 0 1 100%/3;
+  }
 }
 </style>
