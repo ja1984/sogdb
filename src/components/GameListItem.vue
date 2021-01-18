6c834af8c1714015405e92e5e600d94a06301d56
Jonathan Andersson <ja1984@gmail.com>
2021-01-18 18:00:30 +0100

    Update languages for spitlings

--- src/components/GameListItem.vue
+++ src/components/GameListItem.vue
@@ -129,7 +129,8 @@
         </div>
         <div class="column column--wrap">
           <button @click.stop="$emit('toggle-game-in-my-games', game.slug)" class="toggle-own" :class="{'toggle-own--selected': isOwned}">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+            <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg> -->
           </button>
           <a :href="game.store_link" @click.stop target="_blank">
             <div class="store-icon">
@@ -498,7 +499,7 @@ export default {
   }
 
   &.toggle-own--selected {
-    color: #FFC107;
+    color: #E91E63;
   }
 }
 </style>
--- src/components/GamesLibrary.vue
+++ src/components/GamesLibrary.vue
@@ -45,7 +45,7 @@
               <div class="column column--wrap column--small-gutter">
                 <div class="toolbar">
                                 <div class="column column--wrap column--small-gutter">
-                <div @click="useDarkTheme = !useDarkTheme" class="select-theme">
+                <div @click="useDarkTheme = !useDarkTheme" class="select-theme" :aria-label="useDarkTheme ? 'Change to light theme' : 'Change to dark theme'" data-balloon-pos="down">
                   <template v-if="useDarkTheme">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -91,7 +91,7 @@
                 </div>
               </div>
               <div class="column column--wrap column--small-gutter">
-                <div class="select-theme" @click="showSmallCards = !showSmallCards">
+                <div class="select-theme" @click="showSmallCards = !showSmallCards" :aria-label="showSmallCards ? 'Show 3 cards' : 'Show 4 cards'" data-balloon-pos="down">
                   <svg
                     width="24px"
                     height="24px"
@@ -136,8 +136,14 @@
                 </div>
               </div>
               <div class="column column--wrap column--small-gutter">
-                <div class="select-theme" @click="filterMyGames = !filterMyGames" :class="{'select-theme--active': filterMyGames}">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star select-theme__icon"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
+                <div class="select-theme" @click="filterMyGames = !filterMyGames" :class="{'select-theme--active': filterMyGames}" :aria-label="filterMyGames ? 'Show all games' : 'Show my games only'" data-balloon-pos="down">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart select-theme__icon"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                  <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star select-theme__icon"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg> -->
+                </div>
+              </div>
+              <div class="column column--wrap column--small-gutter" v-if="myGames.length > 0">
+                <div class="select-theme" @click="randomizeGame" aria-label="Start random game" data-balloon-pos="down">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-gift"><polyline points="20 12 20 22 4 22 4 12"></polyline><rect x="2" y="7" width="20" height="5"></rect><line x1="12" y1="22" x2="12" y2="7"></line><path d="M12 7H7.5a2.5 2.5 0 0 1 0-5C11 2 12 7 12 7z"></path><path d="M12 7h4.5a2.5 2.5 0 0 0 0-5C13 2 12 7 12 7z"></path></svg>
                 </div>
               </div>
                 </div>
@@ -161,17 +167,6 @@
                     />
                     <div class="checkbox-box" />
                     <span class="checkbox__label">Show only PRO games</span>
-                  </label>
-                </div>
-                <div class="column">
-                  <label class="checkbox">
-                    <input
-                      v-model="filterMyGames"
-                      type="checkbox"
-                      class="form-input"
-                    />
-                    <div class="checkbox-box" />
-                    <span class="checkbox__label">Show only MY games</span>
                   </label>
                 </div>
               </div>
@@ -686,6 +681,11 @@ export default {
     },
   },
   methods: {
+    randomizeGame() {
+      const item = this.myGames[Math.floor(Math.random() * this.myGames.length)];
+      const game = this.games.find((x) => x.slug === item);
+      this.selectGame(game);
+    },
     fillMyGames() {
       if (typeof window.localStorage === 'undefined') return;
       const myGames = window.localStorage.getItem('sogdb-my-games');
@@ -897,7 +897,7 @@ body.dark-theme {
 }
 
 .select-theme--active {
-  color: #FFC107;
+  color: #E91E63;
 }
 .games-library {
   flex: 1;
